4bb93ce75ec65cf6ae8e79343a7a1972da6de2ab
Jansen Morais <jansenmorais86@gmail.com>
2025-03-22 01:43:12 +0000

    adição de array de strings

---
+++ estudo/arrays_e_vetores_em_c/Arrays-.c
@@ -0,0 +1,32 @@
+ 
+int main() {
+    int numeros[5] = {10, 20, 30, 40, 50};  // Inicialização do array com valores
+ 
+    // Acessando e imprimindo elementos específicos do array
+    printf("O primeiro elemento é %d\n", numeros[0]);
+    printf("O terceiro elemento é %d\n", numeros[2]);
+    printf("O quinto elemento é %d\n", numeros[4]);
+
+
+    float notas[3] = {85.5, 90.0, 78.0};  // Array de notas de 3 alunos
+ 
+    printf("Nota do primeiro aluno: %.1f\n", notas[0]);
+    printf("Nota do segundo aluno: %.1f\n", notas[1]);
+    printf("Nota do terceiro aluno: %.1f\n", notas[2]);
+
+    char letras[4] = {'A', 'B', 'C', 'D'};  // Array de caracteres
+ 
+    printf("Primeira letra: %c\n", letras[0]);
+    printf("Segunda letra: %c\n", letras[1]);
+    printf("Terceira letra: %c\n", letras[2]);
+    printf("Quarta letra: %c\n", letras[3]);
+
+    char *nomes[] = {"Alice", "Bob", "Carol"}; // Array de strings
+    int i;
+    // Uma estrutura de repetição para pecorres todos os valores
+    for(i = 0; i < 3; i++) { // 
+        printf("%s\n", nomes[i]);
+    }
+ 
+    return 0;
+}
