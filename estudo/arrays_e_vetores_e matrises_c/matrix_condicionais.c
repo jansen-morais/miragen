555de48fd1ea03f0b6cfe9e0818a685ad224c35b
Jansen Morais <jansenmorais86@gmail.com>
2025-03-26 02:52:41 +0000

    adição de controle

--- estudo/arrays_e_vetores_e matrises_c/matrix_condicionais.c
+++ estudo/arrays_e_vetores_e matrises_c/matrix_condicionais.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
+// Constantes
+#define LINHA 5
+#define COLUNA 5
  
 int main() {
-    // Todas as matrizes precisam ser iguais 
-    int matriz1[2][2] = {{1, 2}, {3, 4}};
-    int matriz2[2][2] = {{5, 6}, {7, 8}};
-    int matrizSoma[2][2];
+    int matriz[LINHA][COLUNA];
+    int target = 4;
+    int found = 0;
+    int soma = 1; // Vai atribuir aos indices os valores de acordo com a aplicação
 
-    // Inicialização da matriz usando estruturas de repetição for aninhadas
-    // Somando as duas matrizes
-    for (int i = 0; i < 2; i++) { // Este loop so rodara apoz que o segundo for fizer uma volta completa
-        for (int j = 0; j < 2; j++) { // Este loop lera a coluna, enquanto o antesseçoer lera a linha 
-            matrizSoma[i][j] = matriz1[i][j] + matriz2[i][j]; // Atribuindo valores à matriz
+    for (int i = 0; i < LINHA; i++) {     
+        for (int j = 0; j < COLUNA; j++) { 
+            matriz[i][j] = soma;
+            soma++;
+            printf("%d ", matriz[i][j]);
         }
+        printf("\n");
+    }
+    // Busca condicional do elemento alvo
+    for (int i = 0; i < LINHA; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < COLUNA; j++) {  // Loop interno para as colunas
+            if (matriz[i][j] == target) {
+                printf("Valor %d encontrado na indice (%d, %d)\n", target, i, j);
+                found = 1;
+                break; // finaliza o loop interno
+            }
+        }
+        if (found) break; // Finaliza o loop externo
     }
  
-    // Exibindo a matriz resultante
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("matrizSoma[%d][%d] = %d\n", i, j, matrizSoma[i][j]);
-        }
+    if (!found) {
+        printf("Elemento %d não encontrado na matriz\n", target);
     }
+
  
     return 0;
 }
